38afb4d9bb3e0c8b3ad51cb3a66dd307a08a95c6
sanchit-gandhi <sanchit@huggingface.co>
2024-05-02 15:20:49 +0100

    finish log probs

--- run_pseudo_labelling.py
+++ run_pseudo_labelling.py
@@ -1,15 +1,19 @@
+import json
 import logging
 import os
+import re
 import shutil
 import sys
 from dataclasses import dataclass, field
-from typing import Any, Dict, List, Optional, Union
+from pathlib import Path
+from typing import Dict, List, Optional, Union, Tuple
 
 import numpy as np
 import torch
-from accelerate import Accelerator
+from accelerate import Accelerator, skip_first_batches
 from accelerate.logging import get_logger
 from datasets import DatasetDict, load_dataset
+from huggingface_hub import create_repo, get_full_repo_name
 from torch import nn
 from torch.utils.data import DataLoader
 from tqdm import tqdm
@@ -48,7 +52,7 @@ class ModelArguments:
         default=None,
         metadata={"help": "Where to store the pretrained models downloaded from huggingface.co"},
     )
-    torch_dtype: Optional[str] = field(
+    dtype: Optional[str] = field(
         default="float16",
         metadata={
             "help": (
@@ -164,13 +168,27 @@ class DataArguments:
         default=False,
         metadata={"help": "Overwrite the content of the output directory each time the script is run."},
     )
+    save_steps: Optional[int] = field(
+        default=500,
+        metadata={"help": "Save the generated prompts every save_steps."},
+    )
+    save_total_limit: Optional[int] = field(
+        default=1, metadata={"help": "If a value is passed, will limit the total number of saved checkpoints"}
+    )
+    report_to: Union[None, str, List[str]] = field(
+        default=None, metadata={"help": "The list of integrations to report the results and logs to."}
+    )
+    wandb_project: Optional[str] = field(
+        default="distil-mistral",
+        metadata={"help": "The name of the wandb project."},
+    )
 
 
 def get_quantization_config(model_args: ModelArguments) -> Union[BitsAndBytesConfig, None]:
     if model_args.load_in_4bit:
         compute_dtype = torch.float16
-        if model_args.torch_dtype not in {"auto", None}:
-            compute_dtype = getattr(torch, model_args.torch_dtype)
+        if model_args.dtype not in {"auto", None}:
+            compute_dtype = getattr(torch, model_args.dtype)
 
         quantization_config = BitsAndBytesConfig(
             load_in_4bit=True,
@@ -228,6 +246,82 @@ class DataCollatorCausalLMWithPadding:
 
         return batch
 
+CHECKPOINT_PREFIX = "checkpoint"
+_RE_CHECKPOINT = re.compile(r"^checkpoint-(\d+).json$")
+
+
+def save_checkpoint(output_dir, all_logprobs, step):
+    checkpoint_path = f"{CHECKPOINT_PREFIX}-{step}.json"
+    output_path = os.path.join(output_dir, checkpoint_path)
+    all_logprobs = [float(logprob) for logprob in all_logprobs]
+    with open(output_path, "w") as file:
+        json.dump(all_logprobs, file)
+
+
+def load_checkpoint(checkpoint_path):
+    with open(checkpoint_path, "r") as file:
+        all_logprobs = json.load(file)
+    return all_logprobs
+
+def clean_checkpoints(output_dir):
+    for item in os.listdir(output_dir):
+        item_path = os.path.join(output_dir, item)
+        # Check if it's a directory
+        if os.path.isdir(item_path):
+            # Recursively clean subdirectories
+            clean_checkpoints(item_path)
+            # Remove the empty directory
+            shutil.rmtree(item_path)
+
+def sorted_checkpoints(output_dir=None) -> List[str]:
+    """Helper function to sort saved checkpoints from oldest to newest."""
+    ordering_and_checkpoint_path = []
+
+    glob_checkpoints = [str(x) for x in Path(output_dir).glob(f"{CHECKPOINT_PREFIX}-*")]
+
+    for path in glob_checkpoints:
+        regex_match = re.match(f".*{CHECKPOINT_PREFIX}-([0-9]+)", path)
+        if regex_match is not None and regex_match.groups() is not None:
+            ordering_and_checkpoint_path.append((int(regex_match.groups()[0]), path))
+
+    checkpoints_sorted = sorted(ordering_and_checkpoint_path)
+    checkpoints_sorted = [checkpoint[1] for checkpoint in checkpoints_sorted]
+    return checkpoints_sorted
+
+
+def rotate_checkpoints(save_total_limit=None, output_dir=None) -> None:
+    """Helper function to delete old checkpoints."""
+    if save_total_limit is None or save_total_limit <= 0:
+        return
+    # Check if we should delete older checkpoint(s)
+    checkpoints_sorted = sorted_checkpoints(output_dir=output_dir)
+    if len(checkpoints_sorted) <= save_total_limit:
+        return
+
+    number_of_checkpoints_to_delete = max(0, len(checkpoints_sorted) - save_total_limit)
+    checkpoints_to_be_deleted = checkpoints_sorted[:number_of_checkpoints_to_delete]
+    for checkpoint in checkpoints_to_be_deleted:
+        logger.info(f"Deleting older checkpoint [{checkpoint}] due to args.save_total_limit")
+        os.remove(checkpoint)
+
+
+def get_last_checkpoint(folder) -> Tuple[List, int]:
+    if not os.path.exists(folder) or not os.path.isdir(folder):
+        os.makedirs(folder, exist_ok=True)
+        return [], 0
+    content = os.listdir(folder)
+    checkpoints = [path for path in content if _RE_CHECKPOINT.search(path) is not None]
+    if len(checkpoints) == 0:
+        return [], 0
+    last_checkpoint = os.path.join(folder, max(checkpoints, key=lambda x: int(_RE_CHECKPOINT.search(x).groups()[0])))
+    # Find num steps saved state string pattern
+    pattern = r"checkpoint-(\d+).json"
+    match = re.search(pattern, last_checkpoint)
+    cur_step = int(match.group(1))
+    # load corresponding generated ids
+    all_logprobs = load_checkpoint(last_checkpoint)
+    return all_logprobs, cur_step
+
 
 def main():
     # 1. Parse input arguments
@@ -247,11 +341,30 @@ def main():
         handlers=[logging.StreamHandler(sys.stdout)],
     )
 
-    accelerator = Accelerator()
+    accelerator = Accelerator(log_with=data_args.report_to, project_dir=data_args.output_dir,)
+    accelerator.init_trackers(project_name=data_args.wandb_project)
 
-    if data_args.overwrite_output_dir and os.path.exists(data_args.output_dir) and os.path.isdir(data_args.output_dir):
+    if data_args.overwrite_output_dir and os.path.exists(data_args.output_dir):
         logger.info("Cleaning output dir from previous run...")
-        shutil.rmtree(data_args.output_dir)
+        clean_checkpoints(data_args.output_dir)
+
+    # 5. Handle the repository creation
+    if accelerator.is_main_process:
+        if data_args.output_dir is not None:
+            os.makedirs(data_args.output_dir, exist_ok=True)
+        if data_args.push_to_hub:
+            if data_args.hub_dataset_id is None:
+                repo_name = get_full_repo_name(
+                    Path(data_args.output_dir).absolute().name,
+                    token=model_args.token,
+                )
+            else:
+                repo_name = data_args.hub_dataset_id
+            create_repo(repo_name, exist_ok=True, token=model_args.token)
+            with open(os.path.join(data_args.output_dir, ".gitignore"), "w+") as gitignore:
+                if "wandb" not in gitignore:
+                    gitignore.write("wandb\n")
+    accelerator.wait_for_everyone()
 
     # 3. Load annotated dataset
     logger.info("*** Load annotated dataset ***")
@@ -278,16 +391,14 @@ def main():
             num_proc=data_args.preprocessing_num_workers,
         )
 
-    raw_datasets_features = list(raw_datasets[next(iter(raw_datasets))].features.keys())
-
     if data_args.max_eval_samples is not None:
         for split in raw_datasets:
             raw_datasets[split] = raw_datasets[split].select(range(data_args.max_eval_samples))
 
     # 4. Load pre-trained model
     logger.info("*** Load pretrained model ***")
-    torch_dtype = (
-        model_args.torch_dtype if model_args.torch_dtype in ["auto", None] else getattr(torch, model_args.torch_dtype)
+    dtype = (
+        model_args.dtype if model_args.dtype in ["auto", None] else getattr(torch, model_args.dtype)
     )
     quantization_config = get_quantization_config(model_args)
     current_device = Accelerator().local_process_index if torch.cuda.is_available() else "cpu"
@@ -299,7 +410,7 @@ def main():
         variant=model_args.model_variant,
         trust_remote_code=model_args.trust_remote_code,
         attn_implementation=model_args.attn_implementation,
-        torch_dtype=torch_dtype,
+        torch_dtype=dtype,
         device_map=device_map,
         quantization_config=quantization_config,
         low_cpu_mem_usage=True,
@@ -369,13 +480,17 @@ def main():
 
     @torch.no_grad()
     def eval_step(batch):
-        shifted_input_ids = batch.pop("labels")[..., 1:]
+        shifted_input_ids = batch.get("input_ids")[..., 1:]
+        shifted_labels = batch.pop("labels")[..., 1:]
         shifted_logits = model(**batch).logits[..., :-1, :]
 
         logprobs = nn.functional.log_softmax((shifted_logits * rescale_temperature).float(), dim=-1).to(shifted_logits.dtype)
         logprobs = torch.gather(logprobs, dim=2, index=shifted_input_ids.unsqueeze(-1))
-        sum_logprobs = (logprobs * (shifted_input_ids != -100)).sum(-1)
-        return sum_logprobs
+
+        sum_logprobs = (logprobs.squeeze(-1) * (shifted_labels != -100)).sum(-1)
+        lengths = (shifted_labels != -100).sum(-1)
+        avg_logprobs = sum_logprobs / lengths
+        return avg_logprobs
 
     for split in vectorized_datasets:
         data_loader = DataLoader(
@@ -386,13 +501,32 @@ def main():
             pin_memory=True,
         )
         data_loader = accelerator.prepare(data_loader)
+        total_inference_steps = len(data_loader)
+        progress_bar = tqdm(
+            range(total_inference_steps), desc=" ... ", position=0, disable=not accelerator.is_local_main_process
+        )
+    
+        split_output_dir = os.path.join(data_args.output_dir, split)
+        all_logprobs, cur_step = get_last_checkpoint(split_output_dir)
+    
+        if cur_step > 0:
+            logger.info(f"Resuming {split} from step {cur_step}")
+            # efficiently skip the first n batches
+            data_loader = skip_first_batches(data_loader, cur_step)
+            progress_bar.update(cur_step)
+    
+        while cur_step < total_inference_steps:
+            for batch in data_loader:
+                logprobs = eval_step(batch)
+                logprobs = accelerator.gather_for_metrics(logprobs)
+                all_logprobs.extend(logprobs.cpu().numpy())
 
-        all_logprobs = []
-        for batch in tqdm(data_loader, disable=not accelerator.is_local_main_process):
-            logprobs = eval_step(batch)
-            # drop duplicates in the last batch
-            logprobs = accelerator.gather_for_metrics(logprobs)
-            all_logprobs.extend(logprobs.cpu().numpy())
+                cur_step += 1
+                progress_bar.update(1)
+
+                if (cur_step % data_args.save_steps == 0) or (cur_step == total_inference_steps):
+                    save_checkpoint(split_output_dir, all_logprobs, cur_step)
+                    rotate_checkpoints(data_args.save_total_limit, output_dir=split_output_dir)
 
         vectorized_datasets[split] = vectorized_datasets[split].add_column("logprobs", all_logprobs)
 
@@ -400,7 +534,7 @@ def main():
         vectorized_datasets.save_to_disk(data_args.output_dir)
         if data_args.push_to_hub:
             vectorized_datasets.push_to_hub(
-                data_args.hub_dataset_id,
+                repo_name,
                 config_name=data_args.dataset_config_name if data_args.dataset_config_name is not None else "default",
                 token=model_args.token,
             )
